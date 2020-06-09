20344ff52f8078d75076268e25a5412a688dc164
Zaman Wahid <zamanwahid16@gmail.com>
2020-06-09 17:06:32 +0600

    using a getter

--- dist/app.js
+++ dist/app.js
@@ -100,6 +100,29 @@ class Component {
     }
     ;
 }
+//ProjectItem Class
+class ProjectItem extends Component {
+    constructor(hostId, project) {
+        super('single-project', hostId, false, project.id);
+        this.project = project;
+        this.configure();
+        this.renderContent();
+    }
+    get persons() {
+        if (this.project.people === 1) {
+            return '1 person';
+        }
+        else {
+            return `${this.project.people} persons`;
+        }
+    }
+    configure() { }
+    renderContent() {
+        this.element.querySelector('h2').textContent = this.project.title;
+        this.element.querySelector('h3').textContent = this.persons + ' assigned';
+        this.element.querySelector('p').textContent = this.project.description;
+    }
+}
 // ProjectList Class
 class ProjectList extends Component {
     constructor(type) {
@@ -130,9 +153,7 @@ class ProjectList extends Component {
         const listEl = document.getElementById(`${this.type}-projects-list`);
         listEl.innerHTML = '';
         for (const prjItem of this.assignedProjects) {
-            const listItem = document.createElement('li');
-            listItem.textContent = prjItem.title;
-            listEl.appendChild(listItem);
+            new ProjectItem(this.element.querySelector('ul').id, prjItem);
         }
     }
 }
--- src/app.ts
+++ src/app.ts
@@ -128,6 +128,32 @@ abstract class Component<T extends HTMLElement,U extends HTMLElement> {
 
 }
 
+//ProjectItem Class
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+    
+    private project: Project;
+    get persons() {
+        if (this.project.people === 1) {
+            return '1 person';
+        }
+        else {
+            return `${this.project.people} persons`;
+        }
+    }
+
+    constructor(hostId: string, project: Project) {
+        super('single-project', hostId, false, project.id);
+        this.project = project;
+        this.configure();
+        this.renderContent();
+    }
+    configure() { }
+    renderContent() {
+        this.element.querySelector('h2')!.textContent = this.project.title;
+        this.element.querySelector('h3')!.textContent = this.persons + ' assigned';
+        this.element.querySelector('p')!.textContent = this.project.description;
+    }
+}
 // ProjectList Class
 class ProjectList extends Component<HTMLDivElement, HTMLElement>{
     assignedProjects: Project[];
@@ -162,9 +188,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement>{
         const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
         listEl.innerHTML = '';
         for (const prjItem of this.assignedProjects) {
-            const listItem = document.createElement('li');
-            listItem.textContent = prjItem.title;
-            listEl.appendChild(listItem);
+            new ProjectItem(this.element.querySelector('ul')!.id, prjItem);
         } 
     }
 
@@ -241,8 +265,7 @@ class ProjectInput extends Component<HTMLDivElement, HTMLFormElement>{
             // console.log(title, desc, people);
             this.clearInputs();
         }
-    }
-    
+    }  
 }
 
 const prjInput = new ProjectInput();
