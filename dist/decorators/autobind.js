da0b4b29bcd127089fd82df9a5b6eb27109f2b8b
Zaman Wahid <zamanwahid16@gmail.com>
2020-06-12 00:42:36 +0600

    updated into modules

---
+++ dist/decorators/autobind.js
@@ -0,0 +1,13 @@
+// auto bind decorator
+export function autobind(target, methodName, descriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
+}
+;
---
+++ src/decorators/autobind.ts
@@ -0,0 +1,12 @@
+// auto bind decorator
+export function autobind(target: any, methodName: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    }
+    return adjDescriptor;
+};
